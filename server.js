9ab83961c90456e1c7a43f8abd1e6f600232af57
Ben Rout <benadamrout@gmail.com>
2019-08-20 20:06:10 +0100

    Adds get and post routes for messages to server.js

--- server.js
+++ server.js
@@ -12,11 +12,34 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 const dbUrl = `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@node-messenger-uchpv.gcp.mongodb.net/test?retryWrites=true&w=majority`;
 
-mongoose.connect(dbUrl, err => {
-    console.log("Mongodb connected", err);
+mongoose.connect(dbUrl, { useNewUrlParser: true }, err => {
+    if (err) {
+        console.log("Error", err);
+    } else {
+        console.log("Mongodb connected");
+    }
+
 });
 
-const Message = mongoose.model('Message', { name: String, message: String });
+const MessageSchema = new mongoose.Schema({ name: String, message: String })
+const MessageModel = mongoose.model('Message', MessageSchema);
+
+app.get('/messages', (req, res) => {
+    MessageModel.find({}, (err, messages) => {
+        res.send(messages);
+    })
+})
+
+app.post('/messages', (req, res) => {
+    const message = new MessageModel(req.body);
+    console.log(message);
+    message.save(err => {
+        if (err) {
+            res.sendStatus(500);
+        }
+        res.sendStatus(200);
+    })
+})
 
 const server = app.listen(3000, () => {
     console.log("Server is running on port", server.address().port);
